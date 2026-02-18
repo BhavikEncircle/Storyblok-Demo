18e772f264a643866f4f71cda708b94eb90eb0b2
BhavikEncircle <bhavikp.encircle@gmail.com>
2026-02-18 11:43:39 +0530

    Created Routes for Visual editing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,8 @@
+import { storyblokEditable } from "@storyblok/react/rsc"
 export const Hero = ({ blok }: any) => {
 
-    return <section className="container mx-auto px-4 w-full pt-32 pb-16">
+    return <section className="container mx-auto px-4 w-full pt-32 pb-16" {...storyblokEditable(blok)}>
         <h1 className="text-center text-5xl md:text-7xl font-bold">{blok.headline}</h1>
-        <p className="text-center text-xl md:text-2xl">{blok.content}</p>
+        <p className="text-center text-xl md:text-2xl mt-4">{blok.content}</p>
     </section>
 }
--- components/Page.tsx
+++ components/Page.tsx
@@ -1,6 +1,6 @@
-import { StoryblokServerComponent } from "@storyblok/react/rsc"
+import { StoryblokServerComponent, storyblokEditable } from "@storyblok/react/rsc"
 export const Page = ({ blok }: any) => {
-    return <main>
+    return <main {...storyblokEditable(blok)}>
         {blok.blocks.map((blok: any) => (
             <StoryblokServerComponent blok={blok} key={blok._uid} />
         ))}
